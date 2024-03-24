7d580e348c51a6b31af998da2415b16eb2a21d00
Jospeh <134275356+ItsRon23@users.noreply.github.com>
2024-03-24 19:14:25 +0700

    implementasi design by contract

--- SayaTubeVideo.cs
+++ SayaTubeVideo.cs
@@ -9,6 +9,7 @@ public class SayaTubeVideo
 
     public SayaTubeVideo(string title)
     {
+        Contract.Requires(title != null && title.Length <= 100, "Judul video harus memiliki panjang max 100 karakter dan tidak boleh null.");
 
         this.id = GenerateRandomId();
         this.title = title;
@@ -21,7 +22,23 @@ public class SayaTubeVideo
         return random.Next(10000, 99999);
     }
 
-    
+    public void IncreasePlayCount(int count)
+    {
+        Contract.Requires(count > 0 && count <= 10000000, "Input penambahan play count max 10.000.000.");
+        Contract.Ensures(playCount <= int.MaxValue - count, "Penambahan play count melebihi max.");
+
+        try
+        {
+            checked
+            {
+                playCount += count;
+            }
+        }
+        catch (OverflowException)
+        {
+            Console.WriteLine("Error: Penambahan play count melebihi batas maksimal.");
+        }
+    }
 
     public void PrintVideoDetails()
     {
